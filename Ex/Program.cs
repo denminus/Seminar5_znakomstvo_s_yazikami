75af6972dc83e0a3fdc4cbc4c8adf4b490f8ca81
den_minus <den.minus@mail.ru>
2023-01-30 16:17:56 +0300

    Использование логики и или , переведение в двоичное число

---
+++ Ex/Program.cs
@@ -0,0 +1,17 @@
+﻿// Условие существования треугольника
+// АС<AB+BC , BC< AB+AC, AB< BC+AC
+
+Console.WriteLine("Введите длинну стороны АВ");
+int AB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длинну стороны ВС");
+int BC = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длинну стороны CA");
+int CA = Convert.ToInt32(Console.ReadLine());
+
+if ((CA<AB+BC) && (BC< AB+CA) && (AB< BC+CA))
+{
+    Console.WriteLine("Это треугольник"); }
+
+else {
+    Console.WriteLine("Это не треугольник");
+        }
---
+++ Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Написать программу преобразования десятичного числа в двоичное
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int remains = 0;
+int count = 0;
+int temp = number;
+
+while (temp > 0)
+{
+temp = temp / 2;
+count++;
+}
+
+int[] array = new int[count];
+
+for (int i = 0; i < array.Length; i++)
+{
+remains = number % 2;
+array[array.Length - 1 - i] = remains;
+number = number / 2;
+}
+
+Console.Write(String.Join("", array));
